173952b11e850addf948f12b3741550ea0fb5e8e
Giovanni Noisy <gnoisy77@gmail.com>
2020-12-05 23:38:48 -0800

    added navbar component

---
+++ mywebsite/src/componenets/Navbar/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+const Navigation = () => {
+  return (
+    <>
+      <Navbar
+        className="border-bottom"
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+      >
+        <Navbar.Brand href="/">Giovanni Noisy</Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          className="border-0"
+        />
+        <Navbar.Collapse
+          className="justify-content-end"
+          id="responsive-navbar-nav"
+        >
+          <Nav activeKey="/home">
+            <Nav.Item>
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+};
+
+export default Navigation;
